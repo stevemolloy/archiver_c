aca64bef377ef1b0b3a33acf800493d08431dae7
Stephen Molloy <sdmolloy@gmail.com>
2026-01-13 16:59:45 +0100

    Added instrumentation

--- src/main.c
+++ src/main.c
@@ -21,7 +21,22 @@ typedef struct {
   char *search_str;
   bool save_to_file;
   char *filename_arg;
+  bool verbose;
 } InputArgs;
+
+void print_input_args(const InputArgs* args) {
+    printf("Searching for attribute: \"%s\"\n", args->search_str);
+    printf("Start date/time: \"%s\"\n", args->start_str);
+    printf("Stop date/time: \"%s\"\n", args->stop_str);
+    if (args->save_to_file) {
+        printf("Saving to file: \"%s\"\n", args->filename_arg);
+    } else {
+        printf("Not saving to a file\n");
+    }
+    if (args->verbose) {
+        printf("Verbose output was selected\n");
+    }
+}
 
 bool check_input_args(InputArgs inargs) {
   if (inargs.start_str==NULL) return false;
@@ -57,13 +72,21 @@ int main(int argc, char **argv) {
     } else if ((strcmp(arg_str, "--file") == 0) || (strcmp(arg_str, "-f") == 0)) {
       input_args.save_to_file = true;
       input_args.filename_arg = SDM_shift_args(&argc, &argv);
+    } else if ((strcmp(arg_str, "--verbose") == 0)) {
+      input_args.verbose = true;
     } else {
       input_args.search_str = arg_str;
     }
   }
 
+  if (input_args.verbose) {
+      printf("------------------INFO----------------------\n");
+      print_input_args(&input_args);
+      printf("------------------INFO----------------------\n");
+  }
+
   if (!check_input_args(input_args)) {
-    fprintf(stderr, "Incorrect input arguments\n");
+    fprintf(stderr, "ERROR: Incorrect input arguments\n");
     usage(stderr, program_name);
     defered_return(1);
   }
@@ -95,6 +118,10 @@ int main(int argc, char **argv) {
   const char *pass_env_str = "ARCHIVER_PASS";
   const char *db_type = "postgresql://hdb_viewer";
   const char *db_pass = getenv(pass_env_str);
+  if (!db_pass || strlen(db_pass)==0) {
+      printf("ERROR: No password found in the %s environment variable. Please set this variable.\n", pass_env_str);
+      defered_return(1);
+  }
   const char *db_url  = "timescaledb.maxiv.lu.se";
   const char *db_port = "15432";
   const char *db_name = "hdb_machine";
@@ -113,6 +140,10 @@ int main(int argc, char **argv) {
   snprintf(conn_str, conn_str_size, "%s:%s@%s:%s/%s", 
            db_type, db_pass, db_url, db_port, db_name);
 
+  if (input_args.verbose) {
+      printf("INFO: Connection string: %s\n", conn_str);
+  }
+
   conn = PQconnectdb(conn_str);
   if (PQstatus(conn) != CONNECTION_OK) {
     fprintf(stderr, "%s", PQerrorMessage(conn));
@@ -122,8 +153,12 @@ int main(int argc, char **argv) {
   attrs = NULL;
   int num_matching_attrs = get_ids_and_tables(conn, input_args.search_str, &attrs);
   if (num_matching_attrs <= 0) {
-    fprintf(stderr, "Search string not found in DB\n");
+    fprintf(stderr, "ERROR: Search string not found in DB\n");
     defered_return(1);
+  }
+
+  if (input_args.verbose) {
+      printf("INFO: Found %d attribute(s) matching \"%s\"\n", num_matching_attrs, input_args.search_str);
   }
 
   PQclear(res);
@@ -131,6 +166,9 @@ int main(int argc, char **argv) {
   DynDataSetArray data_set_array = {0};
   SDM_ENSURE_ARRAY_MIN_CAP(data_set_array, (size_t)num_matching_attrs);
   for (size_t attr_num=0; attr_num<(size_t)num_matching_attrs; attr_num++) {
+    if (input_args.verbose) {
+        printf("INFO: Querying the database for %s\n", attrs[attr_num].name);
+    }
     get_single_attr_data(conn, attrs[attr_num], &data_set_array.data[attr_num], start_tm, stop_tm);
   }
 
@@ -138,13 +176,13 @@ int main(int argc, char **argv) {
     if (input_args.save_to_file) {
       filename = malloc((strlen(input_args.filename_arg) + 32) * sizeof(char));
       if (filename == NULL) {
-        fprintf(stderr, "Could not allocate memory.\n");
+        fprintf(stderr, "ERROR: Could not allocate memory.\n");
         defered_return(1);
       }
       sprintf(filename, "%s-%04zu.csv", input_args.filename_arg, attr_num+1);
       stream = fopen(filename, "w");
       if (stream == NULL) {
-        fprintf(stderr, "Could not open %s: %s\n", filename, strerror(errno));
+        fprintf(stderr, "ERROR: Could not open %s: %s\n", filename, strerror(errno));
         defered_return(1);
       }
     } else {
